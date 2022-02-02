269e64c0e9ad19395b306a36a39dae4c7c38e71b
Macarius <49547750+MacariusD@users.noreply.github.com>
2022-02-02 17:54:27 -0600

    navbar fixed
    navbar variables corrected and javascript now loads only when the page loads.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link');
+
+window.onload=function(){ //makes sure nothing starts before all the code has been loaded. Prevents null values in eventlisteners.
+    const navToggle = document.querySelector('.nav-toggle');
+    const navLinks = document.querySelectorAll('.nav__link');
 
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});
+    navToggle.addEventListener('click', () => {
+        document.body.classList.toggle('nav-open');
+    });
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            document.body.classList.remove('nav-open');
+        })
     })
-})
+}
